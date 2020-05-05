93033dbb8ff08d015145b6c1c6cdd966fc85eb7c
Ameer Al Issa <ameerwisam97@gmail.com>
2020-05-05 15:04:58 +0300

    Initial commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,176 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button6 = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToOrderColumns = True
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 12)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(990, 102)
+        Me.DataGridView1.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 120)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(134, 51)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Run"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(11, 230)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(135, 23)
+        Me.Button2.TabIndex = 7
+        Me.Button2.Text = "Save"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(12, 204)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(134, 20)
+        Me.TextBox3.TabIndex = 8
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(12, 177)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(134, 21)
+        Me.ComboBox1.TabIndex = 9
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(12, 259)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(134, 23)
+        Me.Button4.TabIndex = 11
+        Me.Button4.Text = "delete"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(191, 148)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 12
+        Me.Button3.Text = "Crystal"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(103, 311)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(96, 96)
+        Me.RichTextBox1.TabIndex = 13
+        Me.RichTextBox1.Text = ""
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(11, 322)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 23)
+        Me.Button5.TabIndex = 14
+        Me.Button5.Text = "Button5"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(436, 121)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(368, 315)
+        Me.PictureBox1.TabIndex = 15
+        Me.PictureBox1.TabStop = False
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(289, 289)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(75, 23)
+        Me.Button6.TabIndex = 16
+        Me.Button6.Text = "Button6"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1011, 469)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,214 @@
+﻿Imports System.Data.OleDb
+Imports CrystalDecisions.CrystalReports.Engine
+Imports System.Drawing.Imaging
+Imports System.Text
+
+Public Class Form1
+    Dim flag As Boolean = False
+    Dim dt As DataTable = New DataTable()
+    Public view As DataView
+    Dim selectedRow As DataGridViewRow
+
+    Private Sub getXlFile()
+
+
+        OpenFileDialog1.Filter = "Excel 2003 Documents (*.xls) | *.xls"
+
+        If OpenFileDialog1.ShowDialog() = DialogResult.OK Then
+            Dim fileName As String
+            fileName = OpenFileDialog1.FileName
+            getExcel(fileName)
+        End If
+
+    End Sub
+
+    Private Sub getExcel(Filename)
+
+        Dim connectionString = String.Format("Provider=Microsoft.Jet.OLEDB.4.0; data source={0}; Extended Properties=""Excel 8.0; HDR = Yes; IMEX = 1""", Filename)
+
+        ' Dim SheetName = InputBox("Enter Sheet Name")
+        Dim adapter As OleDbDataAdapter
+        adapter = New OleDbDataAdapter("select * from [sheet1$]", connectionString)
+
+        Dim Data = New DataTable()
+
+        Dim ds = New DataSet()
+        adapter.Fill(ds, "sheet1")
+        Data = ds.Tables("sheet1")
+        dt = Data
+        DataGridView1.DataSource = Data
+
+
+        ' Form2.Show()
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        getXlFile()
+
+        For Each column In dt.Columns
+            ComboBox1.Items.Add(column.ColumnName)
+        Next
+        ComboBox1.SelectedIndex = 0
+
+    End Sub
+
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        Dim dt As DataTable = New DataTable()
+        For Each col As DataGridViewColumn In DataGridView1.Columns
+            dt.Columns.Add(col.Name)
+        Next
+
+        For Each row As DataGridViewRow In DataGridView1.Rows
+            Dim dRow As DataRow = dt.NewRow()
+
+            For Each cell As DataGridViewCell In row.Cells
+                dRow(cell.ColumnIndex) = cell.Value
+            Next
+
+            dt.Rows.Add(dRow)
+        Next
+        Dim customerTable As DataSet = New DataSet()
+        dt.TableName = "patient"
+        customerTable.Tables.Add(dt)
+        ExcelLibrary.DataSetHelper.CreateWorkbook("MyExcelFile", customerTable)
+        MessageBox.Show("Your file has been saved")
+    End Sub
+
+
+    Private Sub TextBox3_TextChanged(sender As Object, e As EventArgs) Handles TextBox3.TextChanged
+        Dim query = _
+       From order In dt.AsEnumerable() _
+       Where order.Field(Of String)(ComboBox1.SelectedItem.ToString).Contains(TextBox3.Text)
+       Select order
+
+        view = query.AsDataView()
+        DataGridView1.DataSource = view
+
+    End Sub
+
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim selected = DataGridView1.SelectedRows.Count
+        If selected > 0 Then
+            Dim result = MessageBox.Show("Do you want to delete these " & selected & " Rows", "caption", MessageBoxButtons.YesNo)
+            Select Case result
+                Case 6 ' i.e "yes"
+                    For Each row As DataGridViewRow In DataGridView1.SelectedRows
+                        DataGridView1.Rows.Remove(row)
+                    Next
+                Case 7 ' i.e "no"
+                    MessageBox.Show("No any row has been deleted")
+            End Select
+        End If
+        MessageBox.Show("Susuessfilly deleted these " & selected & " Rows")
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim cr = New CrystalReport1()
+        cr.Load("C:\Users\Ameer\Documents\Visual Studio 2013\Projects\MHH\CrystalReport1.rpt")
+
+        '(TextObject)
+        Dim txt1 As TextObject = cr.ReportDefinition.Sections("Section1").ReportObjects("Text1")
+        Dim txt2 As TextObject = cr.ReportDefinition.Sections("Section1").ReportObjects("Text2")
+
+        Dim txt3 As TextObject = cr.ReportDefinition.Sections("Section1").ReportObjects("Text3")
+        Dim txt4 As TextObject = cr.ReportDefinition.Sections("Section1").ReportObjects("Text4")
+
+        Dim txt5 As TextObject = cr.ReportDefinition.Sections("Section1").ReportObjects("Text5")
+        Dim txt6 As TextObject = cr.ReportDefinition.Sections("Section1").ReportObjects("Text6")
+
+        Dim txt7 As TextObject = cr.ReportDefinition.Sections("Section1").ReportObjects("Text7")
+        Dim txt8 As TextObject = cr.ReportDefinition.Sections("Section1").ReportObjects("Text8")
+
+        Dim txt9 As TextObject = cr.ReportDefinition.Sections("Section1").ReportObjects("Text9")
+
+        'Dim picture1 As PictureObject = cr.ReportDefinition.Sections("Section1").ReportObjects("Picture1")
+
+        txt1.Text = Convert.ToString(selectedRow.Cells("movement_type").Value)
+        txt2.Text = Convert.ToString(selectedRow.Cells("amount").Value)
+
+        txt3.Text = Convert.ToString(selectedRow.Cells("currency").Value)
+        txt4.Text = Convert.ToString(selectedRow.Cells("voucher_number").Value)
+
+        txt5.Text = Convert.ToString(selectedRow.Cells("name").Value)
+        txt6.Text = Convert.ToString(selectedRow.Cells("note2").Value)
+
+        txt7.Text = Convert.ToString(selectedRow.Cells("mediator").Value)
+        txt8.Text = Convert.ToString(selectedRow.Cells("note").Value)
+
+        txt9.Text = Convert.ToString(selectedRow.Cells("date").Value)
+        'picture1 = CType(PictureBox1, PictureObject)
+
+
+        Dim frm = New Form()
+        frm.Height = 800
+        frm.Width = 600
+        'TextObject txt2 = (TextObject)cryRpt.ReportDefinition.Sections["Section1"].ReportObjects["TextObject2"]
+
+        Dim crystalReportViewer1 = New CrystalDecisions.Windows.Forms.CrystalReportViewer()
+        crystalReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill
+
+        crystalReportViewer1.ReportSource = cr
+        crystalReportViewer1.Refresh()
+
+        frm.Controls.Add(crystalReportViewer1)
+        frm.ShowDialog()
+
+
+    End Sub
+
+    Private Sub DataGridView1_SelectionChanged(sender As Object, e As EventArgs) Handles DataGridView1.SelectionChanged
+
+
+        If (DataGridView1.SelectedCells.Count > 0) Then
+
+            Dim selectedrowindex As Integer = DataGridView1.SelectedCells(0).RowIndex
+            selectedRow = DataGridView1.Rows(selectedrowindex)
+        End If
+
+
+
+
+
+
+
+
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        RichTextBox1.Text = ""
+
+        For i = 0 To ComboBox1.Items.Count - 1
+            RichTextBox1.Text &= selectedRow.Cells(i).Value & " "
+
+            If Not (i = ComboBox1.Items.Count - 1) Then
+            End If
+
+
+        Next
+        PictureBox1.LoadAsync("http://api.qrserver.com/v1/create-qr-code/?data=" + RichTextBox1.Text + "&size=200x200")
+        Dim unicode = Encoding.Unicode.GetBytes(RichTextBox1.Text)
+        Dim qr As Zen.Barcode.CodeQrBarcodeDraw = Zen.Barcode.BarcodeDrawFactory.CodeQr
+        '  PictureBox1.Image = qr.Draw(unicode.ToString, 500)
+        'Dim qr As Zen.Barcode.CodeQrBarcodeDraw = Zen.Barcode.BarcodeDrawFactory.CodeQr
+        'Try
+        '    PictureBox1.Image = qr.Draw("دعاء الصباح - أباذر الحلواجي | Dua Sabah", 500)
+        'Catch ex As Exception
+
+        'End Try
+
+
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        ' Dim path = "c:\Ameer\Document\Documents\MMU_Report\"
+        Dim sf = New SaveFileDialog
+        sf.FileName = "C:\Users\Ameer\Documents\Visual Studio 2013\Projects\MHH\bin\Debug\a.jpeg"
+        PictureBox1.Image.Save(sf.FileName, ImageFormat.Jpeg)
+
+    End Sub
+End Class
